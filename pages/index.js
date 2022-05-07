cf3f24149900d5da2fa63341b1e374e25380d62b
xtxk090 <timotius.kartawijaya@grainger.com>
2022-05-06 23:11:38 -0700

    feat: make small adjustments

--- pages/index.js
+++ pages/index.js
@@ -42,7 +42,7 @@ export default function Home() {
           <div className="flex flex-col justify-center">
             <h1 className="flex justify-center text-4xl font-bold my-4">Hwang Judo</h1>
             <h1 className="flex justify-center text-neutral-500 text-3xl">황 유도</h1>
-            <div className="flex justify-center my-5">
+            <div className="flex justify-center my-5 text-sm">
               <button onClick={handleClick} className={styles.button}>Apply {'>'}</button>
             </div>
           </div>

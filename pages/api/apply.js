f11e06f42fd0f96c87f78e3cf483e6dca5fd3b44
xtxk090 <timotius.kartawijaya@grainger.com>
2022-06-26 23:05:05 -0700

    feat: add send email feature for /apply

---
+++ pages/api/apply.js
@@ -0,0 +1,69 @@
+
+var nodemailer = require('nodemailer')
+var sgTransport = require('nodemailer-sendgrid-transport');
+
+var options = {
+  auth: {
+      api_key: process.env.SENDGRID_API_KEY
+  }
+}
+
+export default function handler(req, res) {
+
+  if (req.method === 'POST') {
+
+    const { first_name, last_name, email, phone, martial_arts_background, good_fit } = req.body;
+
+    var mailer = nodemailer.createTransport(sgTransport(options));
+
+    var emailContent = {
+      from: `${process.env.GMAIL}`,
+      to: `${process.env.GMAIL}`,
+      subject: `New Application from ${email}`,
+      html: `
+      <p>
+      <h3>Application Received from ${first_name} ${last_name}.</h3>
+      <br>
+      <b>First Name:</b> ${first_name}
+      <br>
+      <b>Last Name:</b> ${last_name}
+      <br>
+      <b>Email:</b> ${email}
+      <br>
+      <b>Phone:</b> ${phone}
+      <br>
+      <br>
+      <b>Martial Arts Background:</b>
+      <br>
+      ${martial_arts_background}
+      <br>
+      <br>
+      <b>Tell us how you could be a good fit:</b>
+      <br>
+      ${good_fit}
+      <br>
+      </p>
+      `,
+    };
+
+    mailer.sendMail(emailContent, function(err, info){
+      if (err) {
+        console.log(err)
+        res.status(400).json({'status': '400: Bad Request'})
+      }
+    })
+
+    res.status(200).json({'status': 'OK'})
+    
+    // emailjs.sendForm(process.env.EMAILJS_SERVICE_ID, process.env.EMAILJS_TEMPLATE_ID, req.body, process.env.EMAILJS_PUBLICKEY)
+    //   .then((result) => {
+    //     // console.log(result.text);
+    //     res.status(200).json({ 'status': 'OK' });
+    //   }, (error) => {
+    //     console.log(error.text);
+    //   });
+  } else {
+    res.status(200).json({ 'status': 'OK' });
+  }
+
+}
--- pages/apply.js
+++ pages/apply.js
@@ -2,9 +2,37 @@ import Navigation from '../components/navigation.js'
 import { motion } from 'framer-motion'
 import Footer from '../components/footer.js'
 
+import React, { useRef } from 'react';
+
 export default function Apply() {
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        
+        const data = {
+            first_name: e.target.first_name.value,
+            last_name: e.target.last_name.value,
+            email: e.target.email.value,
+            phone: e.target.phone.value,
+            martial_arts_background: e.target.martial_arts_background.value,
+            good_fit: e.target.good_fit.value,
+        }
+
+        const response = await fetch('/api/apply', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+
+        const result = await response.json()
+
+        // Need to update alerts here
+        alert(`${result.status}`)
+    }
     return (
-        <>  
+        <>
             <Navigation />
             <motion.div initial="hidden" animate="visible" variants={{
                 hidden: {
@@ -19,14 +47,77 @@ export default function Apply() {
                     }
                 }
             }}>
-            <div className="sm:py-8">
-                <div className="flex flex-col px-8 mb-3" id="Apply">
-                    <h1 className="flex my-12 justify-center text-5xl">Apply</h1>
+                <div className="sm:py-8">
+                    <div className="flex flex-col px-8 mb-3" id="Apply">
+                        <h1 className="flex my-12 justify-center text-5xl">Apply</h1>
+                        <div className="text-center text-md md:mx-24 lg:mx-40">
+                            <p>
+                                Please apply below if you’re interested in joining us on the mat.
+                                Space is limited and membership is not guaranteed.
+                                The admissions committee will review your application and get back to you with an interview.
+                            </p>
 
+                        </div>
+                        <div className="my-10 flex justify-center">
+                            <form onSubmit={handleSubmit} className="w-full max-w-lg" >
+                                <div className="flex flex-wrap -mx-3 mb-6">
+                                    <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                                        <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-first-name">
+                                            First Name
+                                        </label>
+                                        <input id="first-name" name="first_name" type="text" placeholder="Jane" required className="appearance-none block w-full bg-gray-200 text-gray-700 border py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" />
+                                    </div>
+                                    <div className="w-full md:w-1/2 px-3">
+                                        <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-last-name">
+                                            Last Name
+                                        </label>
+                                        <input id="last-name" name="last_name" type="text" placeholder="Doe" required className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" />
+                                    </div>
+                                </div>
+                                <div className="flex flex-wrap -mx-3 mb-6">
+                                    <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                                        <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-first-name">
+                                            Email
+                                        </label>
+                                        <input id="email" type="email" name="email" placeholder="Jane.Doe@gmail.com" required className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"  />
+
+                                    </div>
+                                    <div className="w-full md:w-1/2 px-3">
+                                        <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-last-name">
+                                            Phone
+                                        </label>
+                                        <input id="phone" type="text" name="phone" placeholder="###-###-####" minLength="12" maxLength="12" required className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"  />
+                                    </div>
+                                </div>
+                                <div className="flex flex-wrap -mx-3 mb-6">
+                                    <div className="w-full px-3">
+                                        <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-password">
+                                            Martial Arts Background (if any)
+                                        </label>
+                                        <textarea id="martial-arts-background" type="text" name="martial_arts_background" required rows="7" className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" />
+                                    </div>
+                                </div>
+                                <div className="flex flex-wrap -mx-3 mb-6">
+                                    <div className="w-full px-3">
+                                        <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-password">
+                                            Tell us how you could be a good fit.
+                                        </label>
+                                        <textarea id="good-fit" type="text" name="good_fit" required rows="7" className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" />
+                                    </div>
+                                </div>
+                                {/* <div className="md:flex md:items-center">
+                                    <div className="md:w-2/3"> */}
+                                <button type="submit" className="shadow bg-gray-700 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded">
+                                    Send Application
+                                </button>
+                                {/* </div>
+                                </div> */}
+                            </form>
+                        </div>
+                    </div>
                 </div>
-            </div>
             </motion.div>
-            <Footer/>
+            <Footer />
         </>
 
 
